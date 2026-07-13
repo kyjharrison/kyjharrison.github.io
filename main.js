01083e76f6ed979c98acaa12d60cdcac1601d029
transform8511 <261839629+kyjharrison@users.noreply.github.com>
2026-07-12 22:10:09 -0500

    Built dynamic z-index and right-hand page edges / spines

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 import '@fontsource-variable/recursive/full.css'
-import { marked } from 'marked'
-import { splitFrontmatter } from './utils.js'
-import { slugify } from './utils.js'
 
 const main = document.querySelector('main')
 const metadata = await fetch('/index.json').then(r => r.json())
 const openPanes = new Map()
-
-const SPINE_WIDTH = parseInt(
-    getComputedStyle(document.documentElement).getPropertyValue('--spine-width')
-)
 
 let allowUpdateURL = true 
 function updateURL() {
@@ -25,15 +18,57 @@ function updateTitle() {
         : 'Ky Harrison'
 }
 
-function updateRightOrdinals() {
+function updateOrdinals() {
     const totalPanes = openPanes.size
+    let ordinal = 0
     for (const [slug, pane] of openPanes) {
         if (!pane) continue 
+
+        pane.classList.remove('spine-left-0', 'spine-left-1', 'spine-left-2', 'spine-left-3') 
+        pane.dataset.ordinal = ordinal
+        const spineLeftClass = Math.min(ordinal, 2)
+        pane.classList.add(`spine-left-${spineLeftClass}`)
+        const paneLeftStop = parseFloat(getComputedStyle(pane).left)
+        pane.dataset.leftStop = paneLeftStop
+
         pane.classList.remove('spine-right-0', 'spine-right-1', 'spine-right-2', 'spine-right-3') 
-        const ordinalRight = totalPanes - pane.dataset.ordinal - 1 // offset to zero-index like the main ordinals
+        const ordinalRight = totalPanes - ordinal - 1 // offset to zero-index like the main ordinals
         pane.dataset.ordinalRight = ordinalRight
-        const spineRightClass = Math.min(ordinalRight, 3)
+        const spineRightClass = Math.min(ordinalRight, 2)
         pane.classList.add(`spine-right-${spineRightClass}`)
+        const paneRightStop = parseFloat(getComputedStyle(pane).right)
+        pane.dataset.rightStop = paneRightStop
+
+        ordinal++ // increments ordinal by 1
+    }
+}
+
+function updateStack() {
+    const scrollLeft = main.scrollLeft
+    const scrollRight = scrollLeft + main.clientWidth
+    const panes = [...openPanes.values()]
+    for (const pane of panes) {
+        if(!pane) continue // to skip null reservations
+        const paneWidth = pane.clientWidth
+
+        const ordinal = Number(pane.dataset.ordinal)
+        const paneNaturalLeft = ordinal * paneWidth 
+        const paneLeftStop = Number(pane.dataset.leftStop)
+        const isStuckLeft = scrollLeft > paneNaturalLeft - paneLeftStop
+        pane.classList.toggle('stuckLeft', isStuckLeft)
+        const neighborLeftStop = ordinal > 0 && panes[ordinal - 1] ? Number(panes[ordinal - 1].dataset.leftStop) : 0
+        const overlappingLeft = ordinal > 0 && scrollLeft > (ordinal - 1) * paneWidth - neighborLeftStop
+        pane.classList.toggle('overlappingLeft', overlappingLeft)
+
+        const ordinalRight = Number(pane.dataset.ordinalRight)
+        const paneNaturalRight = (ordinal + 1) * paneWidth
+        const paneRightStop = Number(pane.dataset.rightStop)
+        const isStuckRight = scrollRight < paneNaturalRight + paneRightStop
+        pane.classList.toggle('stuckRight', isStuckRight)
+        pane.style.zIndex = isStuckRight ? -(ordinal + 1) : '' 
+        const neighborRightStop = ordinalRight > 0 && panes[ordinal + 1] ? Number(panes[ordinal + 1].dataset.rightStop) : 0
+        const overlappingRight = ordinalRight > 0 && scrollRight < (ordinal + 2) * paneWidth + neighborRightStop
+        pane.classList.toggle('overlappingRight', overlappingRight)
     }
 }
 
@@ -54,16 +89,13 @@ async function appendPane(slug) {
         const parser = new DOMParser()
         const page = parser.parseFromString(raw, 'text/html')
         const pane = page.querySelector('article')
-        const ordinal = openPanes.size - 1 // offset for reservation
-        pane.dataset.ordinal = ordinal
-        const spineLeftClass = Math.min(ordinal, 3)
-        pane.classList.add(`spine-left-${spineLeftClass}`)
         main.appendChild(pane)
         openPanes.set(slug, pane)
+        updateOrdinals() 
         updateURL()
         updateTitle()
-        pane.scrollIntoView({ behavior: 'smooth', inline: 'nearest' })
-        updateRightOrdinals()
+        main.scrollTo({left: Number(pane.dataset.ordinal) * pane.clientWidth, behavior: 'smooth' })
+//        pane.scrollIntoView({ behavior: 'smooth', inline: 'end' })
     } catch (err) {
         openPanes.delete(slug) // delete reservation if misfire
     } 
@@ -75,20 +107,22 @@ function removePane(slug) {
     pane.remove()
     openPanes.delete(slug)
     updateURL()
-    updateRightOrdinals()
+    updateOrdinals()
+    updateStack()
 }
 
 async function loadFromURL() {
     allowUpdateURL = false // if loading from the URL, trying to push changes back would be circular 
     const urlSlugs = window.location.pathname.split('/').filter(s => s)
     if (urlSlugs.length === 0) { urlSlugs.push('home')}
-    for (const slug of openPanes.keys()) {
+    for (const slug of [...openPanes.keys()]) {
         if (!urlSlugs.includes(slug)) { removePane(slug)}
     }
     for (const slug of urlSlugs) {
         await appendPane(slug)
     }
     updateTitle()
+    updateStack()
     allowUpdateURL = true
 }
 
@@ -105,22 +139,7 @@ main.addEventListener('click', (event) => {
 // listens for forward/back buttons
 window.addEventListener('popstate', loadFromURL)
 
-// the core overlapping spine logic
-main.addEventListener('scroll', () => {
-    const scrollLeft = main.scrollLeft
-
-    for (const [slug, pane] of openPanes) {
-        if(!pane) continue // to skip null reservations
-        const ordinal = pane.dataset.ordinal
-        const paneWidth = pane.offsetWidth
-
-        const overlapping = scrollLeft > (ordinal - 1) * paneWidth
-        pane.classList.toggle('overlappingLeft', overlapping)
-
-        const stickyLeft = parseFloat(getComputedStyle(pane).left)
-        const stuck = scrollLeft >= ordinal * paneWidth - stickyLeft
-        pane.classList.toggle('stuckLeft', stuck)        
-    }
-})
+// listens for scroll events 
+main.addEventListener('scroll', updateStack)
 
 loadFromURL()
